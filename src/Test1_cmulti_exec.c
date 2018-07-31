6dc1a81a8831efe71bde41232cabe22ebc3f9223
viky <vigneshwar.s@timesy.com>
2018-07-31 04:55:42 +0000

    Commit from WINDOWS 10

--- src/Test1_cmulti_exec.c
+++ src/Test1_cmulti_exec.c
@@ -19,7 +19,7 @@ void *thread_func( void * not_used )
 	int p, q, r;	/* variables */
 
 	printf("Starting thread %d\n", (int)not_used);
-	printf("Starting thread GIT commit new %d\n", (int)not_used);
+	printf("Starting thread GIT commit new August %d\n", (int)not_used);
 	sleep(1);
 	for ( i=1; i<100; i++ ) {
 		for ( j=1; j<10; j++ ) {
